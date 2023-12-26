952946f03277bbbbfa4fed2d97b69c64fe327264
Khizar Sheikh <khizarsheikh.012@gmail.com>
2023-12-26 23:26:50 +0500

    Root Components

--- Slack Clone/slack-clone/src/App.jsx
+++ Slack Clone/slack-clone/src/App.jsx
@@ -1,11 +1,36 @@
+import Header from './Components/Header/Header';
+import "./App.css"
+import SideBar from './Components/SideBar/SideBar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import ChatScreen from './Components/ChatScreen/ChatScreen';
 
-import Header from './Header';
+import Login from './Components/Login/login';
+import { useStatevalue } from './Components/Provider/StateProvider';
 
 const App = () => {
+  const [{ user }] = useStatevalue()
+
   return (
-    <div>
-      <Header />
-    </div>
+    <div className="app">
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <SideBar />
+              <Routes>
+                <Route path='/room/:roomId' element={<ChatScreen />} />
+                <Route path='/' element={<>Welcome</>} />
+              </Routes>
+            </div>
+          </>
+        )}
+      </Router>
+
+    </div >
+
   )
 }
 
